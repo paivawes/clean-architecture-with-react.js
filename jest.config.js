fc04e4ea822906feb3b012b12a468b04908a8d3f
paivawes <weslleysilvapaiva@outlook.com>
2022-05-16 16:43:41 -0300

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: ['<rootDir>/src'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{ts,tsx}'
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    }
+}
